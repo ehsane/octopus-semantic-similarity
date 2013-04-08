cd16324a2ab458558953f60a3521cbbffe2403d8
Ehsan Emadzadeh <eemadzadeh@paypal.com>
2013-04-08 15:00:39 -0700

    add initial maven structure

---
+++ src/main/java/octopus/semantic/similarity/App.java
@@ -0,0 +1,13 @@
+package octopus.semantic.similarity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
